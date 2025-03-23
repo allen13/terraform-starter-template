5294de7781a349f923b39653cdd0ae894e1bdd0e
allen13 <kex.allen13@gmail.com>
2025-03-23 17:12:48 -0500

    Add networking module variables

---
+++ modules/networking/variables.tf
@@ -0,0 +1,28 @@
+variable "vpc_cidr_block" {
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "environment" {
+  description = "The environment name (e.g., dev, staging, prod)"
+  type        = string
+}
+
+variable "availability_zones" {
+  description = "A list of availability zones in which to create subnets"
+  type        = list(string)
+  default     = ["us-west-2a", "us-west-2b", "us-west-2c"]
+}
+
+variable "enable_nat_gateway" {
+  description = "Whether to enable NAT Gateway for private subnets"
+  type        = bool
+  default     = false
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default     = {}
+}
