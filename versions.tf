eff1e7e0e4beec3b239121ef4b34eab492150a35
allen13 <kex.allen13@gmail.com>
2025-03-23 17:11:57 -0500

    Add versions.tf with provider constraints

---
+++ versions.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    # Uncomment the provider you want to use
+    # aws = {
+    #   source  = "hashicorp/aws"
+    #   version = ">= 4.0.0"
+    # }
+    # azurerm = {
+    #   source  = "hashicorp/azurerm"
+    #   version = ">= 3.0.0"
+    # }
+    # google = {
+    #   source  = "hashicorp/google"
+    #   version = ">= 4.0.0"
+    # }
+  }
+}
